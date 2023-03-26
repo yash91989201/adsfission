0ddd7f7315ee4da751d8e543a9fed3d18a98960f
yash91989201 <yashraj6370@gmail.com>
2023-03-26 23:23:44 +0530

    style: remove side scrolling in mobile due to sidebar

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -22,28 +22,28 @@ export default function Navbar() {
       duration-500 md:px-0
       ${scroll ? "fixed top-0 left-0 z-50 " : "relative"} bg-primary-700`}
     >
-      <div className="mx-auto flex max-w-6xl items-center justify-between ">
+      <div className="flex items-center justify-between max-w-6xl mx-auto ">
         <div className="flex-1"></div>
-        <div className="hidden items-center justify-between space-x-8 md:flex">
+        <div className="items-center justify-between hidden space-x-8 md:flex">
           <nav>
             <ul className="flex space-x-3 font-semibold text-primary-50">
-              <li className="cursor-pointer rounded-sm px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 rounded-sm cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Home
               </li>
-              <li className="cursor-pointer rounded-sm px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 rounded-sm cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Services
               </li>
-              <li className="cursor-pointer rounded-sm px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 rounded-sm cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Contact
               </li>
-              <li className="cursor-pointer rounded-sm px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 rounded-sm cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 About Us
               </li>
             </ul>
           </nav>
           <Link
             href="#"
-            className="flex items-center justify-center space-x-3 rounded-sm border border-transparent bg-primary-300 px-6 py-3 text-white outline-none transition-all duration-500 hover:border-primary-200 hover:bg-white hover:text-primary-200"
+            className="flex items-center justify-center px-6 py-3 space-x-3 text-white transition-all duration-500 border border-transparent rounded-sm outline-none bg-primary-300 hover:border-primary-200 hover:bg-white hover:text-primary-200"
           >
             <span>Free Consulting</span>
             <BsArrowRight />
@@ -60,9 +60,9 @@ export default function Navbar() {
             menu ? "right-0" : "-right-96"
           }`}
         >
-          <div className="mb-6 flex flex-row-reverse items-center">
+          <div className="flex flex-row-reverse items-center mb-6">
             <button
-              className="border p-2 text-xl text-white"
+              className="p-2 text-xl text-white border"
               onClick={() => setMenu(false)}
             >
               <RxCross2 />
@@ -70,23 +70,23 @@ export default function Navbar() {
           </div>
           <nav>
             <ul className="flex flex-col space-y-3 font-semibold text-primary-50">
-              <li className="cursor-pointer border-b px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 border-b cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Home
               </li>
-              <li className="cursor-pointer border-b px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 border-b cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Services
               </li>
-              <li className="cursor-pointer border-b px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 border-b cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 Contact
               </li>
-              <li className="cursor-pointer border-b px-3 py-2 transition-colors duration-300 hover:bg-primary-50 hover:text-primary-700">
+              <li className="px-3 py-2 transition-colors duration-300 border-b cursor-pointer hover:bg-primary-50 hover:text-primary-700">
                 About Us
               </li>
             </ul>
           </nav>
           <Link
             href="#"
-            className="mt-6 flex items-center justify-center space-x-3 rounded-sm  bg-white px-6 py-3 text-primary-300"
+            className="flex items-center justify-center px-6 py-3 mt-6 space-x-3 bg-white rounded-sm text-primary-300"
           >
             <span>Free Consulting</span>
             <BsArrowRight />
